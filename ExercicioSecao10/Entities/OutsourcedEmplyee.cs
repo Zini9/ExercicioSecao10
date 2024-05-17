7755e4260db8247f8e6f583446d7a58a3a122796
Guilherme Zini <guilhermerosazini@gmail.com>
2024-05-16 21:58:57 -0300

    Adicionado classe OutsourcedEmplyee

---
+++ ExercicioSecao10/Entities/OutsourcedEmplyee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioSecao10.Entities
+{
+    internal class OutsourcedEmplyee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmplyee() 
+        { 
+        }
+
+        public OutsourcedEmplyee(double additionalCharge)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+    }
+}
