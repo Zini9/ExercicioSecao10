880b364762726b97ff9ee5d111c20ff9e95aceb4
Guilherme Zini <guilhermerosazini@gmail.com>
2024-05-22 18:42:56 -0300

    Feita toda interação com usuario

--- ExercicioSecao10/Program.cs
+++ ExercicioSecao10/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ExercicioSecao10
 {
@@ -11,15 +12,17 @@ namespace ExercicioSecao10
     {
         static void Main(string[] args)
         {
-            List<Employee> employees = new List<Employee>();
+            List<Employee> list = new List<Employee>();
 
             Console.Write("Enter the number of employees: ");
-            int reg = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < reg; i++) 
+            for (int i = 1; i <= n; i++) 
             {
                 Console.WriteLine("Employee #" + i + " data:");
+
                 Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
 
                 Console.Write("Name: ");
                 string name = Console.ReadLine();
@@ -27,8 +30,20 @@ namespace ExercicioSecao10
                 Console.Write("Hours: ");
                 int hours = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Value per hour: ");
-                double valuePerHour = double.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'y') //Leitura de funcionário terceirizado
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new OutsourcedEmployee(additionalCharge, name, hours,valuePerHour));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
             }
         }
     }
